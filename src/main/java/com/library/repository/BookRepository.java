2490677dd52123573e96ac5aff1a5445bee15597
Vadym <t.vadyk@gmail.com>
2018-02-25 18:46:39 +0200

    Change some methods in BookRepository. Add all implemented methods to BookRepositoryImpl

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -2,7 +2,7 @@ package com.library.repository;
 
 import com.library.entity.Book;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookRepository extends CrudRepository<Book, Integer> {
@@ -10,7 +10,7 @@ public interface BookRepository extends CrudRepository<Book, Integer> {
     /**
      * Task 1. Подивитись, чи певна книжка доступна
      */
-    Boolean getBookAvailability(int bookId);
+//    Boolean getBookAvailability(int bookId);
 
     /**
      * Task 2.1. Вивести всі книжки по основному автору
@@ -20,15 +20,10 @@ public interface BookRepository extends CrudRepository<Book, Integer> {
     /**
      * Task 2.2. Вивести всі книжки по співавтору
      */
-    List<Book> findByCoAuthor(int authorId);
+    List<Book> findByCoAuthor(int coAuthorId);
 
     /**
-     * Task 4.1. Скільки книжок в бібліотеці, які видані в період незалежності
-     */
-    Long getAmountReleasedDuringIndependence();
-
-    /**
-     * Task 4.2. Показати книги, які видані в період незалежності
+     * Task 4 Показати книги, які видані в період незалежності
      */
     List<Book> findReleasedDuringIndependence();
 
@@ -51,17 +46,26 @@ public interface BookRepository extends CrudRepository<Book, Integer> {
     /**
      * Task 6.1. Отримати найпопулярніші книжки за період
      */
-    List<Book> findBestByPeriod(LocalDate fromTime, LocalDate toTime, int resultListSize);
+    List<Book> findBestBooksByPeriod(LocalDateTime startDate, int resultListSize);
 
     /**
      * Task 6.2. Отримати найнепопулярніші книжки за період
      */
-    List<Book> findWorstByPeriod(LocalDate fromTime, LocalDate toTime, int resultListSize);
+    List<Book> findWorstBooksByPeriod(LocalDateTime startDate, int resultListSize);
+    /**
+     * Task 6.3. Отримати к-сть рентів найпопулярніших книжок за період
+     */
+    List<Long> findBestCountOfRentByPeriod(LocalDateTime startDate, int resultListSize);
+
+    /**
+     * Task 6.4. Отримати к-сть рентів НЕ найпопулярніших книжок за період
+     */
+    List<Long> findWorstCountOfRentByPeriod(LocalDateTime startDate, int resultListSize);
 
     /**
      * Task 10. Вивести по назві книжки
      * кількість екземплярів з інформацією про них (наявна, на руках)
      */
-    Book getCopiesInfo(String bookName);
+    Book findBookByName(String bookName);
     // TODO Тут потрібно повертати не Book, а щось типу BookAvailabilityView, котррий містить bookName, generalCount, availableCount
 }
--- src/main/java/com/library/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/library/repository/impl/BookRepositoryImpl.java
@@ -4,65 +4,156 @@ import com.library.entity.Book;
 import com.library.repository.BookRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.TypedQuery;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 
 @Repository
 public class BookRepositoryImpl extends CrudRepositoryImpl<Book, Integer>
         implements BookRepository {
+    private static final LocalDate INDEPENDENCE_DAY_DATE = LocalDate.of(1991, Month.AUGUST, 24);
 
-    @Override
-    public Boolean getBookAvailability(int bookId) {
-        return null;
-    }
+    /**{@inheritDoc}*/
+//    TODO MAKE LOGIC IN SERVICE LEVEL
+//    @Override
+//    public Boolean getBookAvailability(int bookId) {
+//        return null;
+//    }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Book> findByMainAuthor(int authorId) {
-        return null;
+        String sqlQuery = "SELECT DISTINCT b.name FROM Book b JOIN b.mainAuthor a WHERE a.id = ?1";
+        TypedQuery<Book> query = getEntityManager().createQuery(sqlQuery, Book.class);
+        query.setParameter(1, authorId);
+        return query.getResultList();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public List<Book> findByCoAuthor(int authorId) {
-        return null;
+    public List<Book> findByCoAuthor(int coAuthorId) {
+        String sqlQuery = "SELECT DISTINCT b.name FROM Book b JOIN b.collaborationAuthors a WHERE a.id = ?1";
+        TypedQuery<Book> query = getEntityManager().createQuery(sqlQuery, Book.class);
+        query.setParameter(1, coAuthorId);
+        return query.getResultList();
     }
 
-    @Override
-    public Long getAmountReleasedDuringIndependence() {
-        return null;
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Book> findReleasedDuringIndependence() {
-        return null;
+        String sqlQuery = "SELECT b FROM Book b JOIN FETCH b.copiesList c"
+                +" WHERE c.releaseDate BETWEEN :startDate AND :endDate";
+        TypedQuery<Book> query = getEntityManager().createQuery(sqlQuery,Book.class);
+        query.setParameter("startDate", INDEPENDENCE_DAY_DATE);
+        query.setParameter("endDate", LocalDate.now());
+        return query.getResultList();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Long getRentCount(int bookId) {
-        return null;
+        String sqlQuery = "SELECT COUNT(r.copyOfBook.id) FROM Rent r JOIN r.copyOfBook c JOIN c.book b WHERE b.id = ?1";
+        TypedQuery<Long> query= getEntityManager().createQuery(sqlQuery,Long.class);
+        query.setParameter(1,bookId);
+        return query.getSingleResult();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Long getCopiesRentCount(int bookId) {
-        return null;
+        String sqlQuery = "SELECT COUNT(c.id) FROM Rent r JOIN r.copyOfBook c JOIN c.book b"
+                + " WHERE b.id = ?1 GROUP BY b.id";
+        TypedQuery<Long> query = getEntityManager().createQuery(sqlQuery, Long.class);
+        query.setParameter(1,bookId);
+        return query.getSingleResult();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Double averageReadingTime(int bookId) {
-        return null;
+//        TODO check this query for exeption
+        String sqlQuery = "SELECT AVG (DATEDIFF(r.returnTime, r.borrowingTime)) FROM Rent r"
+                + " JOIN r.copyOfBook c JOIN c.book b WHERE b.id = ?1 AND r.returnTime IS NOT NULL";
+        TypedQuery<Double> query = getEntityManager().createQuery(sqlQuery, Double.class);
+        query.setParameter(1,bookId);
+        return query.getSingleResult();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Book> findBestBooksByPeriod(LocalDateTime startDate, int resultListSize) {
+        String sqlQuery = "SELECT b FROM Rent r JOIN r.copyOfBook c JOIN c.book b"
+                + " WHERE r.borrowingTime BETWEEN :startDate AND :endDate GROUP BY b.id ORDER BY COUNT(r.id) DESC";
+        TypedQuery<Book> query = getEntityManager().createQuery(sqlQuery, Book.class);
+        query.setParameter("startDate", startDate)
+                .setParameter("endDate", LocalDateTime.now())
+                .setMaxResults(resultListSize);
+        return query.getResultList();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Book> findWorstBooksByPeriod(LocalDateTime startDate, int resultListSize) {
+        String sqlQuery = "SELECT b FROM Rent r JOIN r.copyOfBook c JOIN c.book b"
+                + " WHERE r.borrowingTime BETWEEN :startDate AND :endDate GROUP BY b.id ORDER BY COUNT(r.id) ASC";
+        TypedQuery<Book> query = getEntityManager().createQuery(sqlQuery, Book.class);
+        query.setParameter("startDate", startDate)
+                .setParameter("endDate", LocalDateTime.now())
+                .setMaxResults(resultListSize);
+        return query.getResultList();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Long> findBestCountOfRentByPeriod(LocalDateTime startDate, int resultListSize) {
+        String sqlQuery = "SELECT COUNT(r.id) FROM Rent r JOIN r.copyOfBook c JOIN c.book b"
+                + " WHERE r.borrowingTime BETWEEN :startDate AND :endDate GROUP BY b.id ORDER BY COUNT(r.id) DESC";
+        TypedQuery<Long> query = getEntityManager().createQuery(sqlQuery, Long.class);
+        query.setParameter("startDate", startDate)
+                .setParameter("endDate", LocalDateTime.now())
+                .setMaxResults(resultListSize);
+        return query.getResultList();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Long> findWorstCountOfRentByPeriod(LocalDateTime startDate, int resultListSize) {
+        String sqlQuery = "SELECT COUNT(r.id) FROM Rent r JOIN r.copyOfBook c JOIN c.book b"
+                + " WHERE r.borrowingTime BETWEEN :startDate AND :endDate GROUP BY b.id ORDER BY COUNT(r.id) ASC";
+        TypedQuery<Long> query = getEntityManager().createQuery(sqlQuery, Long.class);
+        query.setParameter("startDate", startDate)
+                .setParameter("endDate", LocalDateTime.now())
+                .setMaxResults(resultListSize);
+        return query.getResultList();
     }
 
-    @Override
-    public List<Book> findBestByPeriod(LocalDate fromTime, LocalDate toTime, int resultListSize) {
-        return null;
-    }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public List<Book> findWorstByPeriod(LocalDate fromTime, LocalDate toTime, int resultListSize) {
-        return null;
-    }
-
-    @Override
-    public Book getCopiesInfo(String bookName) {
-        return null;
+    public Book findBookByName(String bookName) {
+        String sqlQuery = "SELECT book FROM Book book WHERE book.name = ?1";
+        TypedQuery<Book> query = getEntityManager().createQuery(sqlQuery, Book.class);
+        query.setParameter(1, bookName);
+        return query.getSingleResult();
     }
 }
