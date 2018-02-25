5e0a60d26eb90a3cc2679ba9046e3e40f434984d
oksanamekh <o.v.mekh@gmail.com>
2018-02-25 02:09:36 +0200

    Added stub implementation to BookRepositoryImpl and ClientRepositoryImpl.

--- src/main/java/com/library/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/library/repository/impl/BookRepositoryImpl.java
@@ -4,8 +4,65 @@ import com.library.entity.Book;
 import com.library.repository.BookRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public class BookRepositoryImpl extends CrudRepositoryImpl<Book, Integer>
         implements BookRepository {
 
+    @Override
+    public Boolean getBookAvailability(int bookId) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findByMainAuthor(String authorFirstName, String authorLastName) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findByCoAuthor(String authorFirstName, String authorLastName) {
+        return null;
+    }
+
+    @Override
+    public Long getAmountReleasedDuringIndependence() {
+        return null;
+    }
+
+    @Override
+    public List<Book> findReleasedDuringIndependence() {
+        return null;
+    }
+
+    @Override
+    public Long getRentCount(int bookId) {
+        return null;
+    }
+
+    @Override
+    public Long getCopiesRentCount(int bookId) {
+        return null;
+    }
+
+    @Override
+    public Double averageReadingTime(int bookId) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findBestByPeriod(LocalDate fromTime, LocalDate toTime, int resultListSize) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findWorstByPeriod(LocalDate fromTime, LocalDate toTime, int resultListSize) {
+        return null;
+    }
+
+    @Override
+    public Book getCopiesInfo(String bookName) {
+        return null;
+    }
 }
--- src/main/java/com/library/repository/impl/ClientRepositoryImpl.java
+++ src/main/java/com/library/repository/impl/ClientRepositoryImpl.java
@@ -1,11 +1,59 @@
 package com.library.repository.impl;
 
+import com.library.entity.Book;
 import com.library.entity.Client;
 import com.library.repository.ClientRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public class ClientRepositoryImpl extends CrudRepositoryImpl<Client, Integer>
         implements ClientRepository {
 
+    @Override
+    public List<Book> findReadBooks(int clientId) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findNotReturnedBooks(int clientId) {
+        return null;
+    }
+
+    @Override
+    public Integer getUsingLibraryDays(int clientId) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findDebtors() {
+        return null;
+    }
+
+    @Override
+    public Double getAverageAge() {
+        return null;
+    }
+
+    @Override
+    public Double getAverageUsingLibraryTime() {
+        return null;
+    }
+
+    @Override
+    public Double getAverageRentsCountByPeriod(LocalDate fromTime, LocalDate toTime) {
+        return null;
+    }
+
+    @Override
+    public Double getAverageAgeByBook(int bookId) {
+        return null;
+    }
+
+    @Override
+    public Double getAverageAgeByAuthor(int authorId) {
+        return null;
+    }
 }
