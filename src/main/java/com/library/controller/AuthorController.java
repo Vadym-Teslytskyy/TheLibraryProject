3b68838a95ba9a7209e83de02a8a565c43aacfb4
oksanamekh <o.v.mekh@gmail.com>
2018-03-01 12:42:32 +0200

    Fixed conflicts in authorView.jsp and AuthorController.jsp. Merged files for author page with Vadym`s files.

--- src/main/java/com/library/controller/AuthorController.java
+++ src/main/java/com/library/controller/AuthorController.java
@@ -3,10 +3,12 @@ package com.library.controller;
 import com.library.entity.Author;
 import com.library.model.view.AuthorView;
 import com.library.service.AuthorService;
+import com.library.service.BookService;
 import com.library.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,22 +19,31 @@ public class AuthorController {
 
     private final ClientService clientService;
 
+    private final BookService bookService;
+
     @Autowired
-    public AuthorController(AuthorService authorService, ClientService clientService) {
+    public AuthorController(AuthorService authorService, ClientService clientService, BookService bookService) {
         this.authorService = authorService;
         this.clientService = clientService;
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/authors")
+    public String showAllAuthors(Model model) {
+        model.addAttribute("authors", authorService.findAll());
+        return "authorFilter";
     }
 
     @RequestMapping("/author/{id}")
-    public String authorView(Model model, @PathVariable Integer id) {
+    public String showAuthorView(Model model, @PathVariable Integer id) {
         Author author = authorService.find(id);
         if (author != null) {
-            Double averageClientsAge = clientService.findAverageAgeByAuthor(author.getId());
+            Double averageClientsAge = clientService.findAverageAgeByAuthor(id);
             AuthorView authorView = new AuthorView(author, averageClientsAge);
             model.addAttribute("authorView", authorView);
             return "authorView";
         } else {
-            return "index"; // TODO Make authors.jsp as bookFilter.jsp and redirect there
+            return "authorFilter";
         }
     }
 }
